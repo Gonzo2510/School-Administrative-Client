8c02da17534cb0b3a8d7712f6ac8001f49841b22
Gonzo2510 <Jagonz@live.com>
2024-05-07 22:03:43 -0500

    added department and instructor

--- server/models.py
+++ server/models.py
@@ -5,38 +5,70 @@ from config import db
 
 # Models go here!
 
+# association table
+student_course = db.Table('student_course_associations',
+     db.Column('student_id', db.Integer, db.ForeignKey('students.id')),
+     db.Column('course_id', db.Integer, db.ForeignKey('course.id'))
+    )
 
 class Student(db.Model):
     __tablename__ = 'students'
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
+
+    # relationship mapping student to related courses
+    courses = db.relationship('Course', secondary=student_course, back_populates = "students")
+    # serialization rules
+    serialize_rules = ('-courses.students', )
+    # add validation
 
     def __repr__(self):
         return '<Student %r>' % self. username 
 
     
-
 class Course(db.Model):
     __tablename__ = 'courses'
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
     description = db.Column(db.String(120), nullable=False)
+
+    # relationship mapping course to related students
+    students = db.relationship('Student', secondary=student_course, back_populates='courses')
+
+    instructor = db.relationship("Instructor", back_populates='courses')
+    # add serialization rules
+    serialize_rules = ('-instructor.course', '-students.courses')
+
+    # add validation
 
     def __repr__(self):
         return '<Course %r>' % self.name 
 
 
+class Department(db.Model):
+    __tablename__ = 'departments'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
 
+    # Add relationships
 
+    def __repr__(self):
+        return f'<Department {self.id}, {self.name}>'
 
+class Instructor(db.Model):
+    __tablename__ = 'instructors'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    
+    # relationships
+    courses = db.relationship('Course', back_populates='instructors')
 
+    # serialization rules
+    serialize_rules = ('-courses.instructor', )
 
-
-
-
+    def __repr__(self):
+        return f'<Instructor {self.id}, {self.name}>'
 
 
 
