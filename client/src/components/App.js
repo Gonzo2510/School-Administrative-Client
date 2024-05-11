b07deef258430f53c6f4845f3b12d2f19cdd13e2
Gonzo2510 <Jagonz@live.com>
2024-05-10 22:02:00 -0500

    added template

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,15 +2,47 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
+
+  const [studentsArr, setStudentsArr] = useState([])
+
 useEffect(() => {
-  // stuff
+  fetch("http://127.0.0.1:5555/students")
+  .then((resp) => resp.json())
+  .then((data) => setStudentsArr(data))
 })
 
 
 
 
 
-  return <h1>Project Client</h1>;
+  return(<> 
+    <h1>Project Client</h1>
+    <div className="container">
+            <h1>High School Course Signup List</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Course Name</th>
+                        <th>Instructor</th>
+                        <th>Available Seats</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Mathematics</td>
+                        <td>John Smith</td>
+                        <td>20</td>
+                        <td><button className="button">Sign Up</button></td>
+                    </tr>
+                    {/* Add more course rows here */}
+                </tbody>
+            </table>
+        </div>
+
+  </>);
 }
 
 export default App;
+
+
