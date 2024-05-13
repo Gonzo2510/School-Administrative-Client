d38e7186573ea0214bc3d0203714090e541e1bef
Jacob Gonzales <Jagonz@live.com>
2024-05-13 18:04:41 -0500

    building navbar / course

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,60 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
+
+function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Students</a></li>
+        <li><a href="#">Courses</a></li>
+        <li><a href="#">Instructors</a></li>
+      </ul>
+    </nav>
+  );
+}
+// export default NavBar;
+
+function Course() {
+  fetch("http://127.0.0.1:5555/courses")
+
+
+  return (
+    <li className="cards_item">
+      <div className="card">
+        <div className="card_title">Course Name</div>
+        <p className="card_description">Course Description</p>
+        <p className="card_instructor">Instructor</p>
+        <p className="card_department">Department</p>
+      </div>
+
+    </li>
+  )
+}
+// export default Course;
+
+
+function CourseContainer() {
+
+  return (
+    <>
+    </>
+  )
+}
+// export default CoursesContainer
+
+
+
+
 function App() {
 
-  const [studentsArr, setStudentsArr] = useState([])
-
-useEffect(() => {
-  fetch("http://127.0.0.1:5555/students")
-  .then((resp) => resp.json())
-  .then((data) => setStudentsArr(data))
-})
-
-
-
-
-
   return(<> 
+    <Header/>
+    <NavBar/>
+    <CourseContainer/>
     <h1>Project Client</h1>
+    <h1>Home</h1>
     <div className="container">
             <h1>High School Course Signup List</h1>
             <table>
