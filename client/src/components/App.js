45f5f76be834990879b2aa68ba80259ef9ddcbad
Jacob Gonzales <Jagonz@live.com>
2024-05-14 18:20:52 -0500

    added instructors fetch

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CourseContainer from "./CourseContainer";
 import NavBar from "./NavBar";
-import Course from "./Course";
 
 
 function App() {
 
   const [coursesArr, setCoursesArr] = useState([])
   const [studentsArr, setStudentsArr] = useState([])
+  const [instructorsArr, setInstructorsArr] = useState([])
 
   useEffect(()=>{
     fetch("http://127.0.0.1:5555/courses")
@@ -21,7 +21,12 @@ function App() {
     .then((response) => response.json())
     .then((data) => setStudentsArr(data))
   }, [])
-
+  
+  useEffect(()=>{
+    fetch("http://127.0.0.1:5555/instructors")
+    .then((response) => response.json())
+    .then((data) => setInstructorsArr(data))
+  }, [])
 
   
   
@@ -33,8 +38,8 @@ function App() {
         <Switch>
             {/* <Route exact path="/" component={Home} /> */}
             {/* <Route path="/login" component={Login} /> */}
-            {/* <Route path="/register" component={Register} /> */}
             <Route path="/coursecontainer" component={CourseContainer} />
+            {/* <Route path="/register" component={Register} /> */}
             {/* <Route path="/logout" component={Logout} /> */}
             {/* <Route path="/profile" component={UserProfile} /> */}
         </Switch>
