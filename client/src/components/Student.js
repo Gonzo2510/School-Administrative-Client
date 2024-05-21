060aa3f61a9a453f973336644cbd37622bcf29f1
Jacob Gonzales <Jagonz@live.com>
2024-05-21 12:28:22 -0500

    add text

--- client/src/components/Student.js
+++ client/src/components/Student.js
@@ -7,7 +7,7 @@ function Student({id, name, email, onDelete }) {
       <ul className="student_card_item">
         <div className="card">
           <div className="card_title">{name}</div>
-          <p className="card_email">{email}</p>
+          <p className="card_email">Email: {email}</p>
           <button button onClick={() => onDelete(id)}>Delete</button>
         </div>
       </ul>
