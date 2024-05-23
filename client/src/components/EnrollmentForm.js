66300ffd9200901b9863b9ff244c64a4c57f755e
Jacob Gonzales <Jagonz@live.com>
2024-05-22 19:16:45 -0500

    formik issues

--- client/src/components/EnrollmentForm.js
+++ client/src/components/EnrollmentForm.js
@@ -1,116 +1,50 @@
-// import React from 'react';
-// import { Formik, Field, Form, ErrorMessage } from 'formik';
-
-// const EnrollmentForm = ({ students = [], courses = [], onSubmit }) => {
-//   return (
-//     <Formik
-//       initialValues={{ studentId: '', courseId: '', grade: '' }}
-//       validate={(values) => {
-//         const errors = {};
-//         if (!values.studentId) {
-//           errors.studentId = 'Required';
-//         } else if (!Number.isInteger(Number(values.studentId))) {
-//           errors.studentId = 'Must be a number';
-//         }
-
-//         if (!values.courseId) {
-//           errors.courseId = 'Required';
-//         } else if (!Number.isInteger(Number(values.courseId))) {
-//           errors.courseId = 'Must be a number';
-//         }
-
-//         if (!values.grade) {
-//           errors.grade = 'Required';
-//         } else if (!/^[A-F]$/.test(values.grade)) {
-//           errors.grade = 'Invalid grade format';
-//         }
-
-//         return errors;
-//       }}
-//       onSubmit={(values, { setSubmitting }) => {
-//         onSubmit(values);
-//         setSubmitting(false);
-//       }}
-//     >
-//       {({ isSubmitting }) => (
-//         <Form>
-//           <div>
-//             <label htmlFor="studentId">Student</label>
-//             <Field as="select" name="studentId">
-//               <option value="" label="Select student" />
-//               {students.map((student) => (
-//                 <option key={student.id} value={student.id}>
-//                   {student.name}
-//                 </option>
-//               ))}
-//             </Field>
-//             <ErrorMessage name="studentId" component="div" />
-//           </div>
-
-//           <div>
-//             <label htmlFor="courseId">Course</label>
-//             <Field as="select" name="courseId">
-//               <option value="" label="Select course" />
-//               {courses.map((course) => (
-//                 <option key={course.id} value={course.id}>
-//                   {course.title}
-//                 </option>
-//               ))}
-//             </Field>
-//             <ErrorMessage name="courseId" component="div" />
-//           </div>
-
-//           <div>
-//             <label htmlFor="grade">Grade</label>
-//             <Field name="grade" type="text" />
-//             <ErrorMessage name="grade" component="div" />
-//           </div>
-
-//           <button type="submit" disabled={isSubmitting}>
-//             Submit
-//           </button>
-//         </Form>
-//       )}
-//     </Formik>
-//   );
-// };
-
-// export default EnrollmentForm;
-
 import React from 'react';
-import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import './EnrollmentForm.css';
 
-const EnrollmentForm = ({ onSubmit }) => {
-  return (
-    <Formik
-      initialValues={{ name: '' }}
-      validate={(values) => {
-        const errors = {};
-        if (!values.name) {
-          errors.name = 'Required';
-        }
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        onSubmit(values);
-        setSubmitting(false);
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Form>
-          <div>
-            <label htmlFor="name">Name</label>
-            <Field name="name" type="text" />
-            <ErrorMessage name="name" component="div" />
-          </div>
+const EnrollmentForm = () => {
+    return (
+        <div>
+            <h1>Enrollment Form</h1>
+            <Formik
+                initialValues={{ firstName: '', lastName: '', email: '' }}
+                validationSchema={Yup.object({
+                    firstName: Yup.string()
+                        .max(15, 'Must be 15 characters or less')
+                        .required('Required'),
+                    lastName: Yup.string()
+                        .max(20, 'Must be 20 characters or less')
+                        .required('Required'),
+                    email: Yup.string()
+                        .email('Invalid email address')
+                        .required('Required'),
+                })}
+                onSubmit={(values, { setSubmitting }) => {
+                    setTimeout(() => {
+                        console.log(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+                    }, 400);
+                }}
+            >
+                <Form>
+                    <label htmlFor="firstName">First Name</label>
+                    <Field name="firstName" type="text" />
+                    <ErrorMessage name="firstName" />
 
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </Form>
-      )}
-    </Formik>
-  );
+                    <label htmlFor="lastName">Last Name</label>
+                    <Field name="lastName" type="text" />
+                    <ErrorMessage name="lastName" />
+
+                    <label htmlFor="email">Email Address</label>
+                    <Field name="email" type="email" />
+                    <ErrorMessage name="email" />
+
+                    <button type="submit">Submit</button>
+                </Form>
+            </Formik>
+        </div>
+    );
 };
 
 export default EnrollmentForm;
