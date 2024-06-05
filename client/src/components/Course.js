39195e5bdbf90649a16b7c497b3e0c2b2dae10bd
Jacob Gonzales <Jagonz@live.com>
2024-06-05 16:19:30 -0500

    show student names on form

--- client/src/components/Course.js
+++ client/src/components/Course.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 
 
 function Course({ description, id, instructorName, departmentName, name, students }) {
+  const studentNames = students.map(student => student.name).join(', ');
+
     return (
       <ul className="course_card_item" data-id={id}>
         <div className="card">
@@ -9,7 +11,7 @@ function Course({ description, id, instructorName, departmentName, name, student
           <p className="card_description">{description}</p>
           <p className="card_instructor">Instructor: {instructorName}</p>
           <p className="card_department">Department: {departmentName}</p> 
-          <p className="card_students">{students}</p>
+          <p className="card_students">Students: {studentNames}</p>
         </div>
       </ul>
     )
