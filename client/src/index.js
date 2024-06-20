50c9ec597d7ab3170fd7216be6fb81b3517c5407
Gonzo2510 <Jagonz@live.com>
2024-06-20 16:29:36 -0500

    remove unworking redux from index

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,7 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import App from "./components/App";
 import "./index.css";
-import { Provider } from 'react-redux';
-import { store } from "./redux/store";
 
 const container = document.getElementById("root");
-if (container) {
-    const root = createRoot(container);
-    root.render(
-        <Provider store={store}>
-            <App />
-        </Provider>
-    );
-} else {
-    console.log('Root container missing in index.html')
-}
+const root = createRoot(container);
+root.render(<App />);
