f791a94e81ddfbf5171e0b102fb75ffa0b18d091
Gonzo2510 <Jagonz@live.com>
2024-07-11 16:05:54 -0500

    update db uir name

--- server/config.py
+++ server/config.py
@@ -11,7 +11,7 @@ import os
 # Instantiate app, set attributes
 app = Flask(__name__)
 class Config:
-    DATABASE_URL = os.getenv('postgresql://project_5_db_4k8v_user:t8oXtYeWj7z28AXSpNAl2EHswpLTaIIX@dpg-cq2pe1g8fa8c73ant3og-a.oregon-postgres.render.com/project_5_db_4k8v')
+    SQLALCHEMY_DATABASE_URI = os.getenv('postgresql://project_5_db_4k8v_user:t8oXtYeWj7z28AXSpNAl2EHswpLTaIIX@dpg-cq2pe1g8fa8c73ant3og-a.oregon-postgres.render.com/project_5_db_4k8v')
 
 # Apply the configuration to the app
 app.config.from_object(Config)
